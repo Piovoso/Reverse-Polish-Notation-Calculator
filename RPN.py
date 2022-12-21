6c979dea17ea30700862bb16e322c1337a558685
Björn Monke <61793599+Piovoso@users.noreply.github.com>
2022-12-21 22:41:34 +0200

    Added Comments

--- RPN.py
+++ RPN.py
@@ -2,38 +2,38 @@ class Stack:
     def Append(list, *args): # Last In First Out
         for i in args: list.append(i)
 
-    def Pop(list, count: int = 1): # Removes first value in list
+    def Pop(list, count: int = 1): # Removes first value in Stack
         for i in range(count): list.pop(0)
 
-    def PopPos(list, position: int = 0):
+    def PopPos(list, position: int = 0): # pops a specific index item in Stack
         list.pop(position)
 
-    def PopRet(list):
+    def PopRet(list): # Pops and removes the first item from the Stack
         item = list.pop(0)
         return item
 
-    def Push(list, *args):
+    def Push(list, *args): # Pushes in a value into the Specific index in Stack
         for i in args: list.insert(0,i)
 
 class Queue:
     def Enqueue(list, *args): # First in First out
         for i in args: list.append(i)
 
-    def ForceQueue(list, position: int = 0 ,*args):
+    def ForceQueue(list, position: int = 0 ,*args): # Forces an item into a Specific index in Queue
         for i in args: list.insert(position,i)
 
-    def Dequeue(list, count: int = 1): # Removes last value in list
+    def Dequeue(list, count: int = 1): # Removes last value in Queue
         for i in range(count): list.pop()
 
-    def Rip(list): # retruns the last value in list, and removes it
+    def Rip(list): # Pops and removes the last item from the Stack
         item = list.pop(len(list)-1)
         return item
     
-    def RipPos(list, position: int = 0):
+    def RipPos(list, position: int = 0): # Pops and removes the item at a specific index from the Stack
         item = list.pop(position)
         return item
 
-class All:
+class All: # Dunno, didn't use it, supposed to be similar to .All(char.isDigit) in C#
     def isDigit(*args):
         try:
             for i in args: int(i)
--- main.py
+++ main.py
@@ -1,57 +1,58 @@
 from RPN import Stack, Queue
 
-
 class calculator:
-    def __init__(self, equation):
+    def __init__(self, expression): # initialising Calculator
         QueueList = []
         StackList = []
-        for char in equation:
-            calculator.ShuntingYard(char, StackList, QueueList)
-        
-        print(f'Stack {StackList}\nQueue {QueueList}')
-        print(calculator.ReversePolishNotation(StackList, QueueList))
+        for char in expression: # Seperating Expression to Characters for processing
+            calculator.ShuntingYard(char, StackList, QueueList) # Passing Required Items to ShuntingYard function
 
+        print(calculator.ReversePolishNotation(StackList, QueueList)) # Prints the returned value from ReversePolishNotation Function on line 27.
 
     def ShuntingYard(char, stack, queue):
-        try:
+        try: # Just here to catch if the string is convertable to integer
             value = int(char)
-            if len(queue) > len(stack): value = str(Queue.Rip(queue)) + str(value)
-            Queue.Enqueue(queue, value)
-        except:
-            if len(queue) <= len(stack): char = str(Stack.PopRet(stack)) + char
+            if len(queue) > len(stack): # Checks if item is a two or more digit number or not by comparing list lenghts
+                value = str(Queue.Rip(queue)) + str(value)
+
+            Queue.Enqueue(queue, value) # Adds the character value into the Queue
+
+        except: # String Conversion exception, Mathematical operator
+            if len(queue) <= len(stack): # Checks if it is the Power operator **
+                char = str(Stack.PopRet(stack)) + char 
             Stack.Append(stack, char)
 
-    def ReversePolishNotation(stack, queue):
+    def ReversePolishNotation(stack, queue): # Actual Calculating
         while True:
-            if len(queue) == 1: return queue[0]
+            if len(queue) == 1: return queue[0] #Returns the Final value left in the stack
 
-            if '**' in stack:
-                index = stack.index('**')
-                math = int(Queue.RipPos(queue, index)) ** int(Queue.RipPos(queue, index))
-                Queue.ForceQueue(queue, index, math)
-                Stack.PopPos(stack, index)
+            if '**' in stack: # Power, following code is the exact same for other mathematical operators
+                index = stack.index('**') # Indexes the Operator (single digit)
+                math = int(Queue.RipPos(queue, index)) ** int(Queue.RipPos(queue, index)) # Does the calculation by Riping out the Corresponding values in the position of Operator
+                Queue.ForceQueue(queue, index, math) # Forces the math value back into it's index value in the queue
+                Stack.PopPos(stack, index) # Removes the Operator from the stack.
 
-            elif '/' in stack or '*' in stack:
-                if '*' in stack and stack.index('*') < stack.index('/'):
-                    index = stack.index('*')
+            elif '/' in stack or '*' in stack: # Equal Mathematical operators
+                if '*' in stack and stack.index('*') < stack.index('/'): # Checks which one comes first in the stack.
+                    index = stack.index('*') # following in stated on lines 30 - 33
                     math = int(Queue.RipPos(queue, index)) * int(Queue.RipPos(queue, index))
                     Queue.ForceQueue(queue, index, math)
                     Stack.PopPos(stack, index)
                 else:
-                    index = stack.index('/')
+                    index = stack.index('/') # following in stated on lines 30 - 33
                     math = int(Queue.RipPos(queue, index)) / int(Queue.RipPos(queue, index))
                     Queue.ForceQueue(queue, index, math)
                     Stack.PopPos(stack, index)
 
-            elif '+' in stack or '-' in stack:
-                if '+' in stack and stack.index('+') < stack.index('-'):
-                    index = stack.index('+')
+            elif '+' in stack or '-' in stack: # Equal Mathematical operators
+                if '+' in stack and stack.index('+') < stack.index('-'): # Checks which one comes first in the stack.
+                    index = stack.index('+') # following in stated on lines 30 - 33
                     math = int(Queue.RipPos(queue, index)) + int(Queue.RipPos(queue, index))
                     Queue.ForceQueue(queue, index, math)
                     Stack.PopPos(stack, index)
 
-                else:
-                    index = stack.index('-')
+                else: 
+                    index = stack.index('-') # following in stated on lines 30 - 33
                     math = int(Queue.RipPos(queue, index)) - int(Queue.RipPos(queue, index))
                     Queue.ForceQueue(queue, index, math)
                     Stack.PopPos(stack, index)
